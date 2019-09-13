ecdb979739d130c1c4fd918f00cd9c48eea82269
Alex Vizcaino <avizcaino@uxland.es>
2019-09-13 12:11:51 +0200

    use zeit build

--- ci/npm/README.md
+++
@@ -1,9 +0,0 @@
-# NPM CI action
-
-This action runs `npm install` and `npm test`.
-
-## Example usage
-
-```
-uses: uxland/actions/ci/npm@v1
-```
--- ci/npm/index.js
+++
@@ -1,8 +0,0 @@
-const { exec } = require('child_process');
-const core = require('@actions/core');
-
-try {
-  exec('npm install');
-} catch (error) {
-  core.setFailed(error.message);
-}
