08edf33fffbc41d26a1f5c6633f5d4cda7d33bf4
Joan Bruguera <jbruguera@uxland.cat>
2023-03-13 10:55:57 +0000

    chore: remove unused action

--- ci/npm/lib/run-install.js
+++
@@ -1,7 +0,0 @@
-module.exports = async tools => {
-  try {
-    await tools.runInWorkspace('npm', ['install'], { stdio: [0, 1, 2] });
-  } catch (err) {
-    throw err;
-  }
-};
--- ci/npm/lib/run-test.js
+++
@@ -1,7 +0,0 @@
-module.exports = async tools => {
-  try {
-    await tools.runInWorkspace('npm', ['test'], { stdio: [0, 1, 2] });
-  } catch (err) {
-    throw err;
-  }
-};
